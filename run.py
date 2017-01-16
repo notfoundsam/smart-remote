bca0dfc94fe660f222dbcb825b0f9503898ce2b0
notfound <notfoundsam@gmail.com>
2017-01-17 00:05:38 +0900

    test

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from app.gpio import led
 if __name__ == '__main__':
 	# print
 	led.setup()
-	app.run(host='0.0.0.0', threaded=True)
-	# app.run(debug=True, host='0.0.0.0', threaded=True)
+	# app.run(host='0.0.0.0', threaded=True)
+	app.run(debug=True, host='0.0.0.0', threaded=True)
 	led.destroy()
