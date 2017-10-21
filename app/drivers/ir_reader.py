9872bc6271654b8fd1b9c3f0c5fa3f9b79dc5f52
notfound <notfoundsam@gmail.com>
2017-10-22 00:50:11 +0900

    test 3

--- app/drivers/ir_reader.py
+++ app/drivers/ir_reader.py
@@ -78,6 +78,7 @@ def read_signal():
             text = '-'.join(result)
 
             print(text, file=sys.stderr)
+            print(result, file=sys.stderr)
 
             with open("ir_tmp_code.txt", "w") as text_file:
                 text_file.write(text)
