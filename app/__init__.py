fe7a843b04968ad7399fe8c6f11faf4bb514fe37
notfound <notfoundsam@gmail.com>
2017-11-12 01:03:25 +0900

    add nrf24l01

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+# from __future__ import print_function
+# import sys
+import time
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -13,3 +16,19 @@ lm.login_view = 'login'
 
 # from app import routes
 from app import routes, models, sockets
+import threading
+from threading import Lock
+
+from drivers import wire_recive
+t1 = None
+thread_lock = Lock()
+
+@app.before_first_request
+def before_first_request():
+    t1 = threading.Thread(target=wire_recive.read_signal)
+    t1.start()
+
+# with thread_lock:
+#     if t1 is None:
+#         t1 = threading.Thread(target=wire_recive.read_signal)
+#         t1.start()
---
+++ app/drivers/wire_recive.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import sys, os
+import time
+
+def read_signal():
+    # Using for development
+    if 'APP_ENV' in os.environ and os.environ['APP_ENV'] == 'development':
+        counter = 0;
+        while True:
+            counter += 1
+            print('Out received message decodes to: Hello - %d' % counter, file=sys.stderr)
+            time.sleep(1)
+    else:
+        import RPi.GPIO as GPIO
+        from lib_nrf24 import NRF24
+        import spidev
+
+    GPIO.setmode(GPIO.BCM)
+
+    pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
+    radio = NRF24(GPIO, spidev.SpiDev())
+    radio.begin(0, 17)
+
+    radio.setPayloadSize(32)
+    radio.setChannel(0x76)
+    radio.setDataRate(NRF24.BR_1MBPS)
+    radio.setPALevel(NRF24.PA_MIN)
+
+    radio.setAutoAck(True)
+    radio.enableDynamicPayloads()
+    radio.enableAckPayload()
+
+    radio.openReadingPipe(1, pipes[1])
+    radio.printDetails()
+    radio.startListening()
+
+    while(1):
+        # ackPL = [1]
+        while not radio.available(0):
+            time.sleep(1 / 100)
+        receivedMessage = []
+        radio.read(receivedMessage, radio.getDynamicPayloadSize())
+        print("Received: {}".format(receivedMessage))
+
+        print("Translating the receivedMessage into unicode characters")
+        string = ""
+        for n in receivedMessage:
+            # Decode into standard unicode set
+            if (n >= 32 and n <= 126):
+                string += chr(n)
+        print("Out received message decodes to: {}".format(string))
