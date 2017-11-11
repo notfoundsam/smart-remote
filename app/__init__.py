e045bb979a3e08992c7e969b8af8f117274c5977
notfound <notfoundsam@gmail.com>
2017-11-12 01:08:03 +0900

    off nrf24l01

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,3 @@
-# from __future__ import print_function
-# import sys
-import time
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -14,19 +11,14 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-# from app import routes
 from app import routes, models, sockets
 import threading
-from threading import Lock
-
 from drivers import wire_recive
-t1 = None
-thread_lock = Lock()
 
-@app.before_first_request
-def before_first_request():
-    t1 = threading.Thread(target=wire_recive.read_signal)
-    t1.start()
+# @app.before_first_request
+# def before_first_request():
+#     t1 = threading.Thread(target=wire_recive.read_signal)
+#     t1.start()
 
 # with thread_lock:
 #     if t1 is None:
