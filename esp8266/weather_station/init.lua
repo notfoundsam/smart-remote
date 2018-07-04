bf76c1fc0fde8d666528dbdeaadac0cfe91ced96
notfound <notfoundsam@gmail.com>
2018-07-04 23:21:45 +0900

    add battery voltage

--- esp8266/weather_station/init.lua
+++ esp8266/weather_station/init.lua
@@ -1,11 +1,15 @@
 local dht_pin = 4
+local vin_ratio = 0.0995
+
+adc.force_init_mode(adc.INIT_ADC)
 
 local w = require('wifimodule')
 local m = require('mqttmodule')
 
-tmr.create():alarm(10000, tmr.ALARM_AUTO, function(t)
+tmr.create():alarm(5000, tmr.ALARM_AUTO, function(t)
   if w.isConnected() and m.connected then
     dht22()
+    battery()
   else
     print('offline...\n')
     m.connect()
@@ -22,4 +26,11 @@ function dht22()
   elseif status == dht.ERROR_TIMEOUT then
     print( "DHT timed out." )
   end
+end
+
+function battery()
+  input = adc.read(0)
+  
+  vin = ((input * 3.3) / 1023) / vin_ratio
+  m.publishBattery('{"batValue":'..vin..'}')
 end
--- esp8266/weather_station/mqttmodule.lua
+++ esp8266/weather_station/mqttmodule.lua
@@ -2,10 +2,10 @@ do
   local M = {}
 
   M.connected = false
-  M.server_ip = "192.168.100.10"
+  M.server_ip = "192.168.100.50"
   M.server_port = 1883
   M.server_channel = "/weather"
-  M.device_id = "station_1"
+  M.device_id = "station_outside"
 
   local client = mqtt.Client(M.device_id, 60)
   client:lwt('/offline', M.device_id)
@@ -31,6 +31,10 @@ do
 
   function M.publish(message)
     client:publish(M.server_channel, '['..message..',{"id":"'..M.device_id..'"}]', 0, 0)
+  end
+
+  function M.publishBattery(message)
+    client:publish("/battery", '['..message..',{"id":"'..M.device_id..'"}]', 0, 0)
   end
 
   return M
