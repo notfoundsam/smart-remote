2ef504231d8749a996e2a2d1acdea7d1dc710d5a
Zazimko Alexey <notfoundsam@gmail.com>
2018-10-18 10:43:54 +0900

    update config

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ FROM python:3
 RUN mkdir /app
 WORKDIR /app
 
-RUN pip3 install -U pip
+# RUN pip3 install -U pip
 # RUN pip install -U setuptools
 # RUN pip install -U wheel
 
 COPY requirements.txt ./
-RUN pip3 install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
--- app/bootstrap.py
+++ app/bootstrap.py
@@ -19,34 +19,35 @@ class Config:
     if 'APP_DOCKER' in os.environ:
         self.NODE_RED_HOST = 'node-red'
         self.NODE_RED_PORT = 9090
-        self.DB_HOST = 'db'
-        self.DB_PORT = '3306'
-        self.DB_NAME = 'smart_remote'
-        self.DB_USER = 'root'
-        self.DB_PASS = 'root'
+        self.DB_HOST       = 'db'
+        self.DB_PORT       = '3306'
+        self.DB_NAME       = 'smart_remote'
+        self.DB_USER       = 'root'
+        self.DB_PASS       = 'root'
     else:
         self.NODE_RED_HOST = '192.168.100.100'
         self.NODE_RED_PORT = 9090
-        self.DB_HOST = '192.168.100.100'
-        self.DB_PORT = '3390'
-        self.DB_NAME = 'smart_remote'
-        self.DB_USER = 'root'
-        self.DB_PASS = 'root'
+        self.DB_HOST       = '192.168.100.100'
+        self.DB_PORT       = '3390'
+        self.DB_NAME       = 'smart_remote'
+        self.DB_USER       = 'root'
+        self.DB_PASS       = 'root'
 
     # DiscoverService settings
-    self.BROADCAST_MASK = '255.255.255.255'
-    self.BROADCAST_PORT = 32000
-    self.BROADCAST_INTERVAL = 5
+    self.BROADCAST_MASK      = '255.255.255.255'
+    self.BROADCAST_PORT      = 32000
+    self.BROADCAST_INTERVAL  = 5
 
     # RpiNode settings
     self.SOCKET_BIND_ADDRESS = ''
-    self.SOCKET_BIND_PORT = 32001
-    self.SOCKET_CONNECTIONS = 5
+    self.SOCKET_BIND_PORT    = 32001
+    self.SOCKET_CONNECTIONS  = 5
 
-    self.app.config['TRAP_HTTP_EXCEPTIONS'] = True
-    self.app.config['CSRF_ENABLED'] = True
-    self.app.config['SECRET_KEY'] = 'you-will-never-guess'
-    self.app.config['SQLALCHEMY_DATABASE_URI'] = self.createDbUri()
+    self.app.config['TRAP_HTTP_EXCEPTIONS']           = True
+    self.app.config['CSRF_ENABLED']                   = True
+    self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    self.app.config['SECRET_KEY']                     = 'you-will-never-guess'
+    self.app.config['SQLALCHEMY_DATABASE_URI']        = self.createDbUri()
 
   def createDbUri(self):
       return 'mysql+mysqlconnector://%s:%s@%s:%s/%s' % (self.DB_USER,self.DB_PASS,self.DB_HOST,self.DB_PORT,self.DB_NAME)
